291229ea0df3bccfe3c93f4190f9b2441a308ef9
vovanmix <singingcode@gmail.com>
2015-09-14 16:46:36 -0700

    initial commit

--- src/Vovanmix/Laravel5BillingBraintree/BillingBraintree.php
+++ src/Vovanmix/Laravel5BillingBraintree/BillingBraintree.php
@@ -4,7 +4,10 @@ namespace Vovanmix\Laravel5BillingBraintree;
 
 use Braintree_Configuration;
 use Braintree_ClientToken;
+use Braintree_Customer;
+use Braintree_Subscription;
 use Config;
+use League\Flysystem\Exception;
 
 class BillingBraintree {
 
@@ -38,12 +41,45 @@ class BillingBraintree {
 		return $clientToken;
 	}
 
-	public function makeForm(){
-
+	public function createClient($clientData){
+		$result = Braintree_Customer::create([
+			'firstName' => $clientData['name'],
+			'paymentMethodNonce' => $clientData['nonce']
+		]);
+		if ($result->success) {
+			return $result->customer->id;
+		} else {
+			foreach($result->errors->deepAll() AS $error) {
+				throw new Exception($error->code . ": " . $error->message . "\n");
+			}
+		}
+		return false;
 	}
 
-	public function subscribe(){
+	public function createSubscription($customer_id, $plan_id){
+		$result = Braintree_Customer::find($customer_id);
+		$the_token = null;
+		if ($result->success) {
+			$the_token = $result->customer->paymentMethods[0]->token;
+		} else {
+			foreach($result->errors->deepAll() AS $error) {
+				throw new Exception($error->code . ": " . $error->message . "\n");
+			}
+		}
 
+		$result = Braintree_Subscription::create([
+			'paymentMethodToken' => $the_token,
+			'planId' => $plan_id,
+//			'firstBillingDate' => ''
+		]);
+		if ($result->success) {
+			return $result->subscription->id;
+		} else {
+			foreach($result->errors->deepAll() AS $error) {
+				throw new Exception($error->code . ": " . $error->message . "\n");
+			}
+		}
+		return false;
 	}
 
 	public function checkSubscription(){
